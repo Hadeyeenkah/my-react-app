110baba7f6bac9feb7773db19fbbc8a9cd4a5c4b
cassandra18 <cassandralelei013@gmail.com>
2024-07-19 02:27:23 +0300

    updated the backend and frontend signup logic

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,102 @@
+
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../dbModels/userSchema');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '30d'})
+};
+
+
+const userController =  {
+
+    createUser:  asyncHandler(async(req, res) => {
+
+        const { username, email, password } = req.body;
+
+
+        if(!username || !email || !password) {
+            res.status(400);
+            throw new Error('All fields are required!');
+        };
+
+
+
+        const userExist = await User.findOne({ email });
+
+        if(userExist) {
+            res.status(400);
+            throw new Error('User already exist')
+        }
+        
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            username, email, password: hashedPassword
+        });
+
+        
+        if(newUser) {
+            res.status(201).json({
+                authorId: newUser.id,
+                username: newUser.username,
+                email: newUser.email,
+                token: generateToken(newUser.id)
+            })
+
+
+        } else {
+            res.status(400);
+            throw new Error('Invalid data');
+        };
+
+    }),
+
+    loginUser: asyncHandler(async (req, res) => {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+            res.json({
+
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                token: generateToken(user._id)
+            })
+        }
+        else {
+            res.status(400)
+            throw new Error('Invalid credentials')
+        }
+    }),
+     
+    deleteUser: asyncHandler(async (req, res) => {
+        const userId = req.params.userId;
+
+        const user = await User.findByIdAndDelete(userId);
+
+        if(user) {
+            res.status(200).json({message: 'User deleted successfully'})
+        
+        } else {
+            res.status(404)
+            throw new Error('User not found:', error);
+        }
+    }),
+
+    getMe: asyncHandler(async(req,res) => {
+        res.status(201).json(req.user);
+    })
+
+};
+
+
+
+
+module.exports = userController;
--- Backend/index.js
+++ Backend/index.js
@@ -1,101 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = process.env.PORT || 3000;
 const colors = require('colors');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/errorHandler');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Create connection to MySQL database
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+connectDB();
 
-// Connect to MySQL database
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Connected to database'.blue.rainbow);
-});
+app.use('/api/users', require('./routes/userRoute'));
 
-// Serve static files
-app.use(express.static('public'));
-
-// Register a new user
-app.post('/api/user/register', (req, res) => {
-  const { username, email, password } = req.body;
-  console.log('Recieved data:', req.body);
-  console.log('Password:', password);
-
-  const saltRounds = 10; // Number of salt rounds
-  let hashedPassword;
-
-  try {
-    hashedPassword = bcrypt.hashSync(password, saltRounds);
-  } catch (error) {
-    console.error('Error hashing password:', error);
-    return res.status(500).send('Failed to register user');
-  }
-
-  const uuid = uuidv4();
-  
-  db.query(
-    'INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)',
-    [uuid, username, email, hashedPassword],
-    (err, result) => {
-      if (err) {
-        console.error('Failed to register user:', err);
-        res.status(500).send('Failed to register user');
-      } else {
-        console.log('User registered successfully');
-        res.status(200).send('User registered');
-      }
-    }
-  );
-});
-
-// login a user
-app.post('/api/user/login', (req, res) => {
-  const { email, password } = req.body;
-  
-  db.query(
-    'SELECT * FROM users WHERE email = ?',
-    [email],
-    (err, result) => {
-      if (err) {
-        console.error('Failed to login:', err);
-        res.status(500).send('Failed to login');
-      } else {
-        if (result.length === 0) {
-          res.status(401).send('User not found');
-        } else {
-          const user = result[0];
-          if (bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-              expiresIn: '1h'
-            });
-            res.status(200).send({ token });
-          } else {
-            res.status(401).send('Invalid email or password');
-          }
-        }
-      }
-    }
-  );
-});
-
-
+app.use(errorHandler)
 app.listen(port, () => {
   console.log(`Server running on port ${port}`.magenta.italic.bold);
 }

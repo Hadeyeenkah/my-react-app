f46797d91093437ca6ffa15eedf4a027784e2983
cassandra18 <cassandralelei013@gmail.com>
2024-07-21 16:35:35 +0300

    solved the cors error

--- Backend/index.js
+++ Backend/index.js
@@ -9,13 +9,21 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/errorHandler');
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin:'https://my-react-5grlsarfj-cassandra18s-projects.vercel.app',
   optionsSuccessStatus: 200
 };
 
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  res.on('finish', () => {
+    console.log(`Response headers: ${JSON.stringify(res.getHeaders())}`);
+  });
+  next();
+});
 
 connectDB();
 

ce6a47bee694121beb86e09c4e540fdee0d85b58
cassandra18 <cassandralelei013@gmail.com>
2024-07-21 17:05:07 +0300

    solved the cors error

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,24 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 3000;
 const colors = require('colors');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/errorHandler');
+
+
+const port = process.env.PORT || 3000;
+const app = express();
+connectDB();
+
 
 const corsOptions = {
   origin:'https://my-react-5grlsarfj-cassandra18s-projects.vercel.app',
   optionsSuccessStatus: 200
 };
 
-
 app.use(cors(corsOptions));
+app.use(express.json());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -25,13 +29,20 @@ app.use((req, res, next) => {
   next();
 });
 
-connectDB();
+
 
 app.options('*', cors(corsOptions));
 
 app.use('/api/users', require('./routes/userRoute'));
 
-app.use(errorHandler)
+app.use(errorHandler);
+
+// Backend start link
+app.get('/', (req, res) => {
+
+  res.send('Welcome to My Blog App Backend side');
+
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`.magenta.italic.bold);
 }
